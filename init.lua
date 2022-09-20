d9d7a722db376c0a22a3805bbff669829fbceeab
28n <28nh04@gmail.com>
2022-09-20 14:54:14 +0200

    feat(*): initial commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+require('sierra.base')
+require('sierra.highlights')
+require('sierra.maps')
+-- require('sierra.plugins')
+
+local has = function(x)
+	return vim.fn.has(x) == 1
+end
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+	require('sierra.macos')
+end
+if is_win then
+	require('sierra.windows')
+end

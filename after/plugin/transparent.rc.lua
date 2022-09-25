a9f4c8cd3d5f7640a9373f0d3c977b90f6769447
28n <28nh04@gmail.com>
2022-09-25 22:13:30 +0200

    feat(plugins): add nvim-transparent
    added nvim-transparent to make neovim transparent

---
+++ after/plugin/transparent.rc.lua
@@ -0,0 +1,9 @@
+local status, transparent = pcall(require, "transparent")
+if not status then
+  return
+end
+
+transparent.setup({
+  enable = true,
+  extra_groups = { "NvimTreeNormal", "NvimTreeVertSplit" },
+})
--- lua/sierra/plugins.lua
+++ lua/sierra/plugins.lua
@@ -114,4 +114,6 @@ packer.startup(function(use)
     'folke/todo-comments.nvim',
     requires = 'nvim-lua/plenary.nvim'
   }
+  
+  use 'xiyaowong/nvim-transparent'
 end)

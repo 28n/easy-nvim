485244a7b672944827b6880d310af4c0f1c46319
28n <28nh04@gmail.com>
2022-10-04 15:15:25 +0200

    make config barebones

--- after/plugin/transparent.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, transparent = pcall(require, "transparent")
-if not status then
-  return
-end
-
-transparent.setup({
-  enable = true,
-  extra_groups = { "NvimTreeNormal", "NvimTreeVertSplit" },
-})

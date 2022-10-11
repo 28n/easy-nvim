e2665c8afe7b316e8dcec1e6715f12373b3a8e76
28n <28nh04@gmail.com>
2022-10-11 21:01:54 +0200

    feat(theme): remove gh-theme

--- after/plugin/github-theme.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, github_theme = pcall(require, "github-theme")
-if (not status) then
-  return
-end
-
-github_theme.setup({
-  theme_style = "dark",
-  sidebars = { "qf", "vista_kind", "terminal", "packer", "neo-tree" },
-})

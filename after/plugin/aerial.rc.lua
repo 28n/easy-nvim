2576c8e869615e02f1079b8356e8ca5d5ea36568
28n <28nh04@gmail.com>
2022-11-07 22:03:00 +0100

    feat(aerial): removed unused on_attach
    n

--- after/plugin/aerial.rc.lua
+++ after/plugin/aerial.rc.lua
@@ -3,13 +3,4 @@ if not status then
   return
 end
 
-local keymap = vim.api.nvim_buf_set_keymap
-
-aerial.setup{
-  on_attach = function(bufnr)
-    --[[ keymap(bufnr, 'n', '<Space>a', ':AerialToggle!<Return>', {silent = true})
-    keymap(bufnr, 'n', '<Space>k', ':AerialPrev<Return>', {silent = true})
-    keymap(bufnr, 'n', '<Space>j', ':AerialNext<Return>', {silent = true}) ]]
-    -- keymap(bufnr, 'n', '<Space>q', ':AerialClose<Return>', {silent = true})
-  end
-}
+aerial.setup{}
